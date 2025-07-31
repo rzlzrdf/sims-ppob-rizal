ed64a8b37ed7f0d6ff8ec0168ba905d7c637ceb1
rzlzrdf <rizallazu@student.ub.ac.id>
2025-07-31 15:30:37 +0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +14,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
 })
